46e5c56851266776cf7bdf979612563ccf7c21c8
Tammam Khan <tammam.a.khan@gmail.com>
2022-03-20 19:38:08 -0700

    added user input form functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-var countryInput, graphTypeInput, dataTypeInput, newsAPIKey, newsLink
+var countryInput = "";
+var graphTypeInput = "";
+var dataTypeInput = "";
+var newsAPIKey = "";
+var newsLink = "";
 
+var submitEl = document.querySelector("#submit-form");
+
+var inputFormHandler = function () {
+  // Get the user input values
+  var country = document.querySelector("select[name='country']").value;
+  var graphType = document.querySelector("select[name='graph-type']").value;
+  var dataType = document.querySelector("select[name='data-type']").value;
+
+  // Update global variables
+  countryInput = country;
+  graphTypeInput = graphType;
+  dataTypeInput = dataType;
+
+  // Save user input to local storage
+  saveUserInput();
+};
 
 /////////////////////////////////
 //           NEWS API          //
@@ -7,48 +27,49 @@ var countryInput, graphTypeInput, dataTypeInput, newsAPIKey, newsLink
 
 function getNewsAPI() {
   var requestOptions = {
-    method: 'GET'
+    method: "GET",
   };
-  
+
   var params = {
-    q: 'covid',
-    pageSize: '5',
+    q: "covid",
+    pageSize: "5",
     // apiKey: '4c2773d12e684f40b1fe0aa0e6487e74'
   };
-  
+
   var esc = encodeURIComponent;
   var query = Object.keys(params)
-    .map(function(k) {return esc(k) + '=' + esc(params[k]);})
-    .join('&');
-  
+    .map(function (k) {
+      return esc(k) + "=" + esc(params[k]);
+    })
+    .join("&");
+
   fetch("https://newsapi.org/v2/everything?" + query, requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .then(data => {
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .then((data) => {
       newsCardCreator(data);
-  })
-  .catch(error => console.log('error', error));
+    })
+    .catch((error) => console.log("error", error));
 }
 
-function updateNews() {
-
-}
+function updateNews() {}
 
 function newsCardCreator(data) {
-  var cardTitle = document.getElementById('card_img_1')
+  var cardTitle = document.getElementById("card_img_1");
 }
 
+var saveUserInput = function () {
+  localStorage.setItem("country", JSON.stringify(countryInput));
+  localStorage.setItem("graphType", JSON.stringify(graphTypeInput));
+  localStorage.setItem("dataType", JSON.stringify(dataTypeInput));
+};
 
-/////////////////////////////////
-//            MAIN             //
-/////////////////////////////////
+var loadUserInput = function () {
+  var savedCountry = localStorage.getItem("country");
+  var savedGraphType = localStorage.getItem("graphType");
+  var savedDataType = localStorage.getItem("dataType");
+};
 
-function main() {
-  getNewsAPI();
-}
-
-
-
-
-
-main()
+submitEl.addEventListener("click", inputFormHandler);
+getNewsAPI();
+loadUserInput();
